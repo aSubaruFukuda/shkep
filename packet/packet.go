b40a9a0ba4fd8a8429177042215afd9d3e256330
subaruf <subaruf@test.com>
2018-10-08 13:18:24 +0900

    improve readability

--- packet/packet.go
+++ packet/packet.go
@@ -11,22 +11,22 @@ type Field struct {
 	Name   string
 }
 type Packet struct {
-	Fields   []Field
-	ByteSize int
+	Fields    []Field
+	ByteWidth int
 }
 
-func NewPacket(fi []Field, bs int) *Packet {
-	pck := Packet{fi, bs}
+func NewPacket(fi []Field, bw int) *Packet {
+	pck := Packet{fi, bw}
 	return &pck
 }
 func (pck *Packet) writeFirstline() {
 	fmt.Printf("|")
 	byteStr := ""
 	for i := 0; i < 32; i++ {
-		if (i < 10) {
-			byteStr = strings.Repeat(" ", pck.ByteSize/2-1) + strconv.Itoa(i) + strings.Repeat(" ", pck.ByteSize-(pck.ByteSize/2-1)-1)
+		if i < 10 {
+			byteStr = strings.Repeat(" ", pck.ByteWidth/2-1) + strconv.Itoa(i) + strings.Repeat(" ", pck.ByteWidth-(pck.ByteWidth/2-1)-1)
 		} else {
-			byteStr = strings.Repeat(" ", pck.ByteSize/2-1) + strconv.Itoa(i) + strings.Repeat(" ", pck.ByteSize-(pck.ByteSize/2-1)-2)
+			byteStr = strings.Repeat(" ", pck.ByteWidth/2-1) + strconv.Itoa(i) + strings.Repeat(" ", pck.ByteWidth-(pck.ByteWidth/2-1)-2)
 		}
 		fmt.Printf("%s", byteStr)
 		fmt.Printf("|")
@@ -39,12 +39,12 @@ func (pck *Packet) writeFreeFields() {
 	for _, v := range pck.Fields {
 		for i := 0; i < v.Length; i++ {
 			if i == v.Length-1 {
-				fmt.Printf("%s", strings.Repeat(" ", pck.ByteSize)+"|")
+				fmt.Printf("%s", strings.Repeat(" ", pck.ByteWidth)+"|")
 			} else {
-				fmt.Printf("%s", strings.Repeat(" ", pck.ByteSize+1))
+				fmt.Printf("%s", strings.Repeat(" ", pck.ByteWidth+1))
 			}
 			cnt += 1
-			if 32 == cnt {
+			if cnt == 32 {
 				fmt.Printf("\n|")
 				cnt = 0
 			}
@@ -56,13 +56,13 @@ func (pck *Packet) writerFields() {
 	fmt.Printf("|")
 	fieldStr := ""
 	for _, v := range pck.Fields {
-		fieldStr = strings.Repeat(" ", (v.Length*(pck.ByteSize+1)-len(v.Name))/2) + v.Name + strings.Repeat(" ", (v.Length*(pck.ByteSize+1) - (v.Length*(pck.ByteSize+1)-len(v.Name))/2))
-		for i := 0; i < (v.Length * (pck.ByteSize + 1)); i += (pck.ByteSize + 1) {
-			if (i + pck.ByteSize + 1) == (v.Length * (pck.ByteSize + 1)) {
-				fmt.Printf(fieldStr[i : i+pck.ByteSize])
+		fieldStr = strings.Repeat(" ", (v.Length*(pck.ByteWidth+1)-len(v.Name))/2) + v.Name + strings.Repeat(" ", (v.Length*(pck.ByteWidth+1)-(v.Length*(pck.ByteWidth+1)-len(v.Name))/2))
+		for i := 0; i < (v.Length * (pck.ByteWidth + 1)); i += (pck.ByteWidth + 1) {
+			if (i + pck.ByteWidth + 1) == (v.Length * (pck.ByteWidth + 1)) {
+				fmt.Printf(fieldStr[i : i+pck.ByteWidth])
 				fmt.Printf("|")
 			} else {
-				fmt.Printf(fieldStr[i : i+pck.ByteSize+1])
+				fmt.Printf(fieldStr[i : i+pck.ByteWidth+1])
 			}
 			cnt++
 			if cnt == 32 {
